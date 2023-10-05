115b2b62dcf979a23558ee708c7a0038e039ef2a
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 14:50:13 +0700

    Tugas 9 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas9.js
@@ -0,0 +1,14 @@
+function Kellurahan() {
+  var Warga = {
+    nama: "Budi",
+    umur: 24,
+    alamat: "jl Jambu Gedungdoro no 45b",
+    status: "Menikah",
+    nik: 3285639957300003,
+  };
+  for (var x in Warga) {
+    console.log(Warga[x]);
+  }
+}
+
+Kellurahan();
